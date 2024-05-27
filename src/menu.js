e859ad2345ecc677215c3715a3a79306c08b502e
aaronjudell <ajudell@gmail.com>
2024-05-27 09:02:40 -0400

    Add more items to menu.

--- src/menu.js
+++ src/menu.js
@@ -1,4 +1,6 @@
 import PenneIMG from './img/penne.jpeg';
+import ChickenIMG from './img/chicken.jpeg';
+import FishIMG from './img/fish.jpeg';
 
 function createMenu() {
     let mainDiv = document.getElementById('content');
@@ -18,8 +20,29 @@ function createMenu() {
     PenneDescript.textContent = 'Our homemade penne with a delicious creamy vodka sauce.';
     PenneDiv.append(PenneImage, PenneTitle, PennePrice, PenneDescript);
 
+    let ChickenDiv = document.createElement('div');
+    let ChickenImage = document.createElement('img');
+    ChickenImage.src = ChickenIMG;
+    let ChickenTitle = document.createElement('h3');
+    ChickenTitle.textContent = 'Chicken Parmesan';
+    let ChickenPrice = document.createElement('span');
+    ChickenPrice.textContent = '23.99';
+    let ChickenDescript = document.createElement('p');
+    ChickenDescript.textContent = 'Breaded chicken cutlet covered in mozzarella cheese with marinara sauce. Served with spaghetti.';
+    ChickenDiv.append(ChickenImage, ChickenTitle, ChickenPrice, ChickenDescript);
+    
+    let FishDiv = document.createElement('div');
+    let FishImage = document.createElement('img');
+    FishImage.src = FishIMG;
+    let FishTitle = document.createElement('h3');
+    FishTitle.textContent = 'Fish and Chips';
+    let FishPrice = document.createElement('span');
+    FishPrice.textContent = '20.99';
+    let FishDescript = document.createElement('p');
+    FishDescript.textContent = 'Breaded fresh cod and french fries. Side of tartar sauce.';
+    FishDiv.append(FishImage, FishTitle, FishPrice, FishDescript);
 
-    mainDiv.append(menuHeadline, PenneDiv);
+    mainDiv.append(menuHeadline, PenneDiv, ChickenDiv, FishDiv);
     
 }
 
